eebcdd5b0d6344268676dbe4cdf2dd58cc413658
ChrisWhite <chriswhite33333@gmail.com>
2024-03-28 17:18:42 +0800

    Add singleton pattern to Runtime class

--- utils/runtime.py
+++ utils/runtime.py
@@ -1,6 +1,8 @@
 from utils.preLoader import PreLoader
 from utils.msgQueue import MsgQueue
+from singleton_decorator import singleton
 
+@singleton
 class Runtime():
     def __init__(self) -> None:
         self.preLoader = PreLoader()
