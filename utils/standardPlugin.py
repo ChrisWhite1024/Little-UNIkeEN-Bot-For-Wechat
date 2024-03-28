0b65eea5de60393ad382ab8dff152bc9836418a4
ChrisWhite <chriswhite33333@gmail.com>
2024-03-28 17:18:24 +0800

    Add PluginType enum to standardPlugin.py

--- utils/standardPlugin.py
+++ utils/standardPlugin.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Union, Tuple, Any
 from utils.runtime import Runtime
+from enum import Enum
 
 class StandardPlugin(ABC):
     @staticmethod
@@ -20,3 +21,8 @@ class StandardPlugin(ABC):
     def scheduleEvent(runtime:Runtime) -> Union[None, str]:
         """
         """
+class PluginType(Enum):
+    GROUP = 1
+    USER = 2
+    BOTH = 3
+    DISABLED = 4
